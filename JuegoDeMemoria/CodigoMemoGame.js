75abb0dfd3dd957e81c292e35c17770074967b0c
maxuis <99999132+maxuis@users.noreply.github.com>
2024-05-26 16:00:15 -0300

    Update CodigoMemoGame.js

--- JuegoDeMemoria/CodigoMemoGame.js
+++ JuegoDeMemoria/CodigoMemoGame.js
@@ -2,20 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const ImagenDeFondo = document.getElementById('Imagen-Fondo');
     const Menu = document.getElementById('Menu');
     const Juego = document.getElementById('Juego');
-    const BotonJugar = document.getElementById('Boton-Jugar');
     const TableroJuego = document.getElementById('Tablero-Juego');
+    const BotonJugar = document.getElementById('Boton-Jugar');    
     const BotonResetear = document.getElementById('Resetear');
+    const BotonMusica = document.getElementById('Musica');
+    const gyroButton = document.getElementById('gyro-button');        
     let Cartas = [];
     let CartasVolteadas = [];
     let CartasEncontradas = 0;
     let UltimaCartaVolteada = null;
     let JuegoIniciado = false;
-    const BotonMusica = document.getElementById('Musica');
-    let MusicaActiva = false;
+    let gyroEnabled = false;
+    let MusicaActiva = false;            
     const cardValues = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    let cardSet = cardValues.concat(cardValues); // Duplicate each card
-
-    const sounds = [
+    let cardSet = cardValues.concat(cardValues);
+    const Musicas = [
         new Audio('Sonidos/Sonido1.mp3'),
         new Audio('Sonidos/Sonido2.mp3'),
         new Audio('Sonidos/Sonido3.mp3'),
@@ -26,8 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Verificar si la música está habilitada
         if (MusicaActiva) {
             // Si la música está habilitada, reproducir sonido aleatorio
-            const randomIndex = Math.floor(Math.random() * sounds.length);
-            const sound = sounds[randomIndex];
+            const randomIndex = Math.floor(Math.random() * Musicas.length);
+            const sound = Musicas[randomIndex];
             sound.play();
             sound.onended = () => {
                 // Si el sonido termina, verificar si la música aún está habilitada antes de reproducir otro sonido
@@ -36,17 +37,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             };
         }
-    }    
+    } 
+    
+    function ReproducirSonidoBoton() {
+        const clickSound = new Audio('Sonidos/Sonido6.mp3');
+        clickSound.play();
+    }
+    
     BotonMusica.addEventListener('click', () => {
+        ReproducirSonidoBoton();
         if (MusicaActiva) {
             // Si la música está activada, detenerla            
             BotonMusica.textContent = 'Musica NO';
-            MusicaActiva = false;
+            MusicaActiva = false;            
             ReproducirSonidoAleatorio();
         } else {
             // Si la música está desactivada, reproducirla            
             BotonMusica.textContent = 'Musica SI';
-            MusicaActiva = true;
+            MusicaActiva = true;            
             ReproducirSonidoAleatorio();
         }
     });
@@ -70,6 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function InvertirCarta(card, value) {
+        ReproducirSonidoBoton();
         if (CartasVolteadas.length < 2 && !card.classList.contains('flipped')) {
             card.classList.add('flipped');
             CartasVolteadas.push({ card, value });
@@ -89,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }    
 
-    function ResetearJuego() {                
+    function ResetearJuego() {                      
         CartasEncontradas = 0;
         CartasVolteadas = [];
         UltimaCartaVolteada = null;
@@ -100,14 +109,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 500);
     }
 
-    function IniciarJuego() {               
+    function IniciarJuego() {                    
         BarajarCarta(cardSet);
         Cartas = cardSet.map(value => CrearCarta(value));
         Cartas.forEach(card => TableroJuego.appendChild(card));
         JuegoIniciado = true;  // Empezar Juego        
     }
 
-    BotonJugar.addEventListener('click', () => {
+    BotonJugar.addEventListener('click', () => {  
+        ReproducirSonidoBoton();      
         Menu.style.display = 'none';
         Juego.style.display = 'block';
         JuegoIniciado = false;
@@ -115,10 +125,27 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     BotonResetear.addEventListener('click', () => {
+        ReproducirSonidoBoton();        
         JuegoIniciado = false;  // El juego se reinicia antes de poder volver a iniciarlo.
         ResetearJuego();
     });
 
+    gyroButton.addEventListener('click', () => {
+        if (!gyroEnabled) {
+            let confirmacion = confirm('¿Deseas permitir el uso del giroscopio para girar la imagen de fondo?');
+            if (confirmacion) {
+                gyroEnabled = true;
+                gyroButton.textContent = 'Girar Imagen: Sí';
+                alert('El uso del giroscopio ha sido habilitado.');
+            } else {
+                alert('Has rechazado el uso del giroscopio.');
+            }
+        } else {
+            gyroEnabled = false;
+            gyroButton.textContent = 'Girar Imagen: No';
+            alert('El uso del giroscopio ha sido deshabilitado.');
+        }
+    });
     // Evento Giroscopio para girar la imagen de fondo
     if (window.DeviceOrientationEvent) {
         window.addEventListener('deviceorientation', event => {
